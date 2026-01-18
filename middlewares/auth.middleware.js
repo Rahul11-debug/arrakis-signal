c0a33d1bcc44b97e123a3dcc653e96cf021ce1fc
Rahul11-debug <rahul.kumarundergrade@gmail.com>
2026-01-18 14:58:24 +0530

    add auth middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+export const protect = async (req, res, next) => {
+    const token = req.headers.authorization && req.headers.authorization.startsWith('Bearer ')
+        ? req.headers.authorization.split(' ')[1]
+        : null;
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized: No token provided' });
+    }   
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select('-password');
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+    }
+};
+
