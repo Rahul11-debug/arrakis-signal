6b9b4914c957b228af4b28b7b4254efc6e0e04ea
Rahul11-debug <rahul.kumarundergrade@gmail.com>
2026-01-18 14:25:22 +0530

    server js modified

---
+++ server.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
+console.log("🔥 server.js file loaded");
+
+dotenv.config();
+
+console.log("🔥 env loaded");
+
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+console.log("🔥 about to start server");
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
+
