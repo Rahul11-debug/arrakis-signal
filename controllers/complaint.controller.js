f3ee6477a4e62373f982c44ce15f071823a96de3
Rahul11-debug <rahul.kumarundergrade@gmail.com>
2026-01-19 20:54:14 +0530

    comaplaints added

---
+++ controllers/complaint.controller.js
@@ -0,0 +1,174 @@
+import Complaint from '../models/Complaint.js';
+
+export const createComplaint = async (req, res) => {
+  try {
+    const SLA_HOURS = 72;
+
+    const complaint = await Complaint.create({
+      ...req.body,
+
+      image: req.file
+        ? {
+            url: `/uploads/complaints/${req.file.filename}`,
+            filename: req.file.filename
+          }
+        : null,
+
+      user: req.user._id,
+
+      slaDeadline: new Date(
+        Date.now() + SLA_HOURS * 60 * 60 * 1000
+      )
+    });
+
+    res.status(201).json(complaint);
+  } catch (error) {
+    res.status(500).json({
+      message: 'Complaint creation failed',
+      error: error.message
+    });
+  }
+};
+
+export const myComplaints = async (req, res) => {
+    const data = await Complaint.find({ user: req.user._id });
+    res.json(data);
+};
+
+export const getSingleComplaint = async (req, res) => {
+  try {
+    const complaint = await Complaint.findById(req.params.id)
+      .populate('user', 'name email role')
+      .populate('assignedTo', 'name email role');
+
+    if (!complaint) {
+      return res.status(404).json({
+        message: 'Complaint not found'
+      });
+    }
+    if (
+      req.user.role === 'user' &&
+      complaint.user._id.toString() !== req.user._id.toString()
+    ) {
+      return res.status(403).json({
+        message: 'Access denied'
+      });
+    }
+
+    res.json(complaint);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
+
+export const allComplaints = async (req, res) => {
+    const data = await Complaint.find().populate('user assignedTo', 'name email');
+    res.json(data);
+};
+
+export const updateStatus = async (req, res) => {
+    const complaint = await Complaint.findById(req.params.id);
+    complaint.status = req.body.status || complaint.status;
+    await complaint.save();
+    res.json(complaint);
+};
+
+export const assignComplaint = async (req, res) => {
+  try {
+    const { staffId } = req.body;
+
+    if (!staffId) {
+      return res.status(400).json({
+        message: 'staffId is required'
+      });
+    }
+
+    const staff = await User.findById(staffId);
+    if (!staff || staff.role !== 'staff') {
+      return res.status(400).json({
+        message: 'Invalid staff user'
+      });
+    }
+
+    const complaint = await Complaint.findById(req.params.id);
+    if (!complaint) {
+      return res.status(404).json({
+        message: 'Complaint not found'
+      });
+    }
+
+    complaint.assignedTo = staffId;
+    complaint.status = 'in-progress';
+
+    await complaint.save();
+
+    res.json({
+      message: 'Complaint assigned successfully',
+      complaint
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updatePriority = async (req, res) => {
+  try {
+    const { priority } = req.body;
+
+    if (!['low', 'medium', 'high'].includes(priority)) {
+      return res.status(400).json({
+        message: 'Invalid priority value'
+      });
+    }
+
+    const complaint = await Complaint.findById(req.params.id);
+    if (!complaint) {
+      return res.status(404).json({
+        message: 'Complaint not found'
+      });
+    }
+
+    complaint.priority = priority;
+    await complaint.save();
+
+    res.json({
+      message: 'Priority updated successfully',
+      complaint
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteComplaint = async (req, res) => {
+  try {
+    const complaint = await Complaint.findById(req.params.id);
+
+    if (!complaint) {
+      return res.status(404).json({
+        message: 'Complaint not found'
+      });
+    }
+
+    await complaint.deleteOne();
+
+    return res.status(200).json({
+      success: true,
+      message: 'Complaint deleted successfully'
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+
+
+export const heatmapData = async (req, res) => {
+    const data = await Complaint.find({}, '{location:1, _id:0}');
+    res.json(data);
+};
---
+++ routes/complaint.routes.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import {
+  createComplaint,
+  myComplaints,
+  allComplaints,
+  updateStatus,
+  heatmapData,
+  getSingleComplaint,
+  assignComplaint,
+  updatePriority,
+  deleteComplaint
+} from '../controllers/complaint.controller.js';
+
+import { protect } from '../middlewares/auth.middleware.js';
+import { authorise } from '../middlewares/role.middleware.js';
+import { upload } from '../middlewares/upload.middleware.js';
+
+const router = express.Router();
+
+router.post('/', protect, upload.single('image'), createComplaint);
+router.get('/me', protect, myComplaints);
+router.get('/', protect, authorise('admin', 'staff'), allComplaints);
+router.get('/:id', protect, getSingleComplaint);
+router.put('/:id/status', protect, authorise('admin', 'staff'), updateStatus);
+router.put('/:id/assign', protect, authorise('admin'), assignComplaint);
+router.delete('/:id', protect, authorise('admin'), deleteComplaint);
+router.put('/:id/priority', protect, authorise('admin'), updatePriority);
+
+router.get('/heatmap', heatmapData);
+
+export default router;
