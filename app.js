208acf4c29e28a8f9acc294c1705133310ff1c8a
Rahul11-debug <rahul.kumarundergrade@gmail.com>
2026-01-19 20:57:17 +0530

    app modified

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/user.routes.js';
+import complaintRoutes from './routes/complaint.routes.js';
+import analyticsRoutes from './routes/analytics.routes.js';
+import slaRoutes from './routes/sla.routes.js';
+import notificationRoutes from './routes/notification.routes.js';
+import publicRoutes from './routes/public.routes.js';
+
+import errorHandler from './middlewares/error.middleware.js';
+import otpRoutes from './routes/otp.routes.js';
+import passwordRoutes from './routes/password.routes.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/api/otp', otpRoutes);
+app.use('/api/password', passwordRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/complaints', complaintRoutes);
+app.use('/api/analytics', analyticsRoutes);
+app.use('/api/sla', slaRoutes);
+app.use('/api/notifications', notificationRoutes);
+app.use('/api/public', publicRoutes);
 
-
-app.get("/", (req, res) => {
-  res.send("App is running");
-});
+app.use(errorHandler);
 
 export default app;
