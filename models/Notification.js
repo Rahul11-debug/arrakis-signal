a8f50e2dde8a799e7d557508607d33549b20237b
Rahul11-debug <rahul.kumarundergrade@gmail.com>
2026-01-20 21:05:45 +0530

    notification model

--- models/Notification.js
+++ models/Notification.js
@@ -1,21 +1,28 @@
 import mongoose from 'mongoose';
 
-const schema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
+const schema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    title: String,
+    message: String,
+    type: {
+      type: String,
+      enum: ['STATUS', 'SLA', 'SYSTEM'],
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+    relatedComplaint: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Complaint',
+    },
   },
-    message: {
-    type: String,
-    required: true,
-  },
-    read: {
-    type: Boolean,
-    default: false,
-  },
-}, {
-  timestamps: true,
-});
+  { timestamps: true }
+);
 
 export default mongoose.model('Notification', schema);
