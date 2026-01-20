193ae623e5aa50e023ff6b56fc1d6c1ff233fb1a
Rahul11-debug <rahul.kumarundergrade@gmail.com>
2026-01-20 21:06:37 +0530

    sla route

--- routes/sla.routes.js
+++ routes/sla.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {overdueComplaints,slaStats } from '../controllers/sla.controller.js';
+import {overdueComplaints,slaStats,slaMonitor } from '../controllers/sla.controller.js';
 import { protect } from '../middlewares/auth.middleware.js';
 import { authorise } from '../middlewares/role.middleware.js';
 
@@ -7,4 +7,5 @@ const router = express.Router();
 
 router.get('/overdue', protect, authorise('admin'), overdueComplaints);
 router.get('/stats', protect, authorise('admin'), slaStats);
+router.get('/monitor', protect, authorise('admin', 'staff'), slaMonitor);
 export default router;
