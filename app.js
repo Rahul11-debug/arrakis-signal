1d517363cba18e7f3416b59e06e0e19e600d7960
Rahul11-debug <rahul.kumarundergrade@gmail.com>
2026-01-21 14:09:29 +0530

    added cors

--- app.js
+++ app.js
@@ -20,7 +20,14 @@ import passwordRoutes from './routes/password.routes.js';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    credentials: true,
+  })
+);
+
 app.use(helmet());
 app.use(morgan('dev'));
 const __filename = fileURLToPath(import.meta.url);
