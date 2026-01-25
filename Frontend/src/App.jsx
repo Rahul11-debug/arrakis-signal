ed288f6fb44082fa019b4161f69b239b104477ef
manish-core-1 <manish.sevdaundergrade@gmail.com>
2026-01-25 16:18:32 +0530

    app.jsx added

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+import OtpVerify from "./pages/auth/OtpVerify";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import ResetPassword from "./pages/auth/ResetPassword";
+
+
+import CitizenDashboard from "./pages/dashboard/CitizenDashboard";
+import StaffDashboard from "./pages/dashboard/StaffDashboard";
+import AdminDashboard from "./pages/dashboard/AdminDashboard";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/verify-otp" element={<OtpVerify />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+
+
+      <Route path="/dashboard/citizen" element={<CitizenDashboard />} />
+      <Route path="/dashboard/staff" element={<StaffDashboard />} />
+      <Route path="/dashboard/admin" element={<AdminDashboard />} />
+    </Routes>
+  );
+}
+
+
