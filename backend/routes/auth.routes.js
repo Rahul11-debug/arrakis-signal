aafcfa9f696ba38a18fd3a7764862a2fefbea96f
Rahul11-debug <rahul.kumarundergrade@gmail.com>
2026-01-26 00:26:11 +0530

    auth route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,12 +1,18 @@
 import express from 'express';
-import {register , login , me , logout } from '../controllers/auth.controller.js';
+import {register , login , getMe , logout } from '../controllers/auth.controller.js';
 import { protect } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/me', protect, me);
+router.get("/me", protect, (req, res) => {
+  res.json({
+    success: true,
+    user: req.user,
+  });
+});
+
 router.post('/logout', protect, logout);
 
 export default router;
