e86f8d043b4a219e457235d072de94eb49425d8e
manish-core-1 <manish.sevdaundergrade@gmail.com>
2026-02-01 22:22:49 +0530

    auth api

--- Frontend/src/api/auth.api.js
+++ Frontend/src/api/auth.api.js
@@ -2,20 +2,20 @@
 import api from "./axios"
 
 export const registerUser = (data) =>
- api.post("api/auth/register", data);
+ api.post("auth/register", data);
 
 export const verifyOtp = (data) =>
-  api.post("api/otp/verify", data);
+  api.post("otp/verify", data);
 
 export const loginUser = (data) =>
-  api.post("api/auth/login", data,
+  api.post("auth/login", data,
     { withCredentials: true,
     }
   );
 
 export const getMe = () => {
   const token = localStorage.getItem("token");
-  return api.get("api/auth/me", {
+  return api.get("auth/me", {
     headers: {
       Authorization: `Bearer ${token}`,
     },
@@ -23,21 +23,21 @@ export const getMe = () => {
 };
 
 export const forgotPassword = (email) =>
-  api.post(`api/password/forgot`, { email });
+  api.post(`password/forgot`, { email });
 
 export const resetPassword = ({ token, password }) => {
-  api.post('api/password/reset/${token}', { password });
+  api.post('password/reset/${token}', { password });
 };
 
 export const resendOtp = (email) => {
-  api.post("api/otp/resend", { email });
+  api.post("otp/resend", { email });
 };
 
 export const changePassword = (data) =>
-  api.put('api/password/change/${data.token}', data);
+  api.put('password/change/${data.token}', data);
 
 export const logout = () => {
-  api.post("api/auth/logout", {}, {
+  api.post("auth/logout", {}, {
     withCredentials: true,
   });
 };
