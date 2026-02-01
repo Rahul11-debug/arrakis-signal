d957f835c955c4b9e60b2b3dc8f1adfbb68c4e1d
manish-core-1 <manish.sevdaundergrade@gmail.com>
2026-02-01 14:23:29 +0530

    auth api

--- Frontend/src/api/auth.api.js
+++ Frontend/src/api/auth.api.js
@@ -1,22 +1,44 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/auth";
+const API = "http://localhost:5000/api";
 
 export const registerUser = (data) =>
-  axios.post(`${API}/register`, data);
+  axios.post(`${API}/auth/register`, data);
 
 export const verifyOtp = (data) =>
   axios.post(`${API}/otp/verify`, data);
 
 export const loginUser = (data) =>
-  axios.post(`${API}/login`, data);
+  axios.post(`${API}/auth/login`, data,
+    { withCredentials: true,
+    }
+  );
+
+export const getMe = () => {
+  const token = localStorage.getItem("token");
+  return axios.get(`${API}/auth/me`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
 
 export const forgotPassword = (email) =>
   axios.post(`${API}/password/forgot`, { email });
 
-export const resetPassword = (data) =>
-  axios.post(`${API}/password/reset`, data);
+export const resetPassword = ({ token, password }) => {
+  return axios.post(`${API}/password/reset/${token}`, { password });
+};
 
 export const resendOtp = (email) => {
-  axios.post(`${API}/register`, { email });
+  axios.post(`${API}/otp/resend`, { email });
+};
+
+export const changePassword = (data) =>
+  axios.put(`${API}/password/change/${data.token}`, data);
+
+export const logout = () => {
+  axios.post(`${API}/auth/logout`, {}, {
+    withCredentials: true,
+  });
 };
