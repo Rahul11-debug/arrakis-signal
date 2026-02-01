ad55af340483f6119b1f94cd7c558733efa66776
manish-core-1 <manish.sevdaundergrade@gmail.com>
2026-02-01 14:24:56 +0530

    authcontext file

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,8 +1,6 @@
 import { createContext, useContext, useState } from "react";
 
-
 const AuthContext = createContext();
-
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
