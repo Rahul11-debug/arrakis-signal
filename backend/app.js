5b85309156d691c071eec38acf134c39344a5ed9
manish-core-1 <manish.sevdaundergrade@gmail.com>
2026-02-01 20:50:00 +0530

    connect deloyed frontend

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
+    process.env.CLIENT_URL,
   ],
   
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
