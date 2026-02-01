386481f8d50119f4eb694630a8aaa27957f48cb0
manish-core-1 <manish.sevdaundergrade@gmail.com>
2026-02-01 20:34:28 +0530

    app.js

--- backend/app.js
+++ backend/app.js
@@ -22,14 +22,14 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 
+  origin: [
     "http://localhost:5173",
+  ],
   
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
 
-app.options("*", cors());
 app.use(helmet());
 app.use(morgan('dev'));
 const __filename = fileURLToPath(import.meta.url);
