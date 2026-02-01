540da79325ec2bed0b0f9a4b942d32b6959dccea
Rahul11-debug <rahul.kumarundergrade@gmail.com>
2026-02-01 13:28:07 +0530

    complaint route

--- backend/routes/complaint.routes.js
+++ backend/routes/complaint.routes.js
@@ -3,8 +3,7 @@ import {
   createComplaint,
   myComplaints,
   allComplaints,
-  updateStatus,
-  heatmapData,
+  updateComplaintStatus,
   assignComplaint,
   updatePriority,
   deleteComplaint,
@@ -12,7 +11,8 @@ import {
   getComplaintHistory,
   filterComplaints,
   getMySingleComplaint,
-  addRemark
+  addRemark,
+  getPublicMapComplaints,
 } from '../controllers/complaint.controller.js';
 import { updateComplaintLifecycle } from '../controllers/complaint.controller.js';
 import { protect } from '../middlewares/auth.middleware.js';
@@ -20,23 +20,27 @@ import { authorise } from '../middlewares/role.middleware.js';
 import { upload } from '../middlewares/upload.middleware.js';
 
 const router = express.Router();
+router.get('/me', protect, myComplaints);
+router.get('/filter', protect, authorise('admin', 'staff'), filterComplaints);
+router.get('/public/map', getPublicMapComplaints);
+
+router.put('/:id/assign', protect, authorise('admin', 'staff'), assignComplaint);
+router.put('/:id/status', protect, authorise('admin', 'staff'), updateComplaintStatus);
+router.put('/:id/priority', protect, authorise('admin'), updatePriority);
+router.put('/:id/lifecycle', protect, authorise('admin', 'staff'), updateComplaintLifecycle);
+router.post('/:id/remarks', protect, authorise('admin', 'staff'), addRemark);
+router.get('/:id/report', protect, authorise('admin', 'staff'), generateComplaintReport);
+router.get('/:id/history', protect, getComplaintHistory);
+
+router.get('/:id', protect, getMySingleComplaint);
 
 router.post('/', protect, upload.single('image'), createComplaint);
-router.get('/me', protect, myComplaints);
-router.get('/', protect, authorise('admin', 'staff'), allComplaints);
-router.get('/:id', protect, getMySingleComplaint);
-router.put('/:id/status', protect, authorise('admin', 'staff'), updateStatus);
-router.put('/:id/assign', protect, authorise('admin'), assignComplaint);
 router.delete('/:id', protect, authorise('admin'), deleteComplaint);
-router.put('/:id/priority', protect, authorise('admin'), updatePriority);
-router.get('/:id/report', protect, authorise('admin', 'staff'), generateComplaintReport);
-router.get('/heatmap', heatmapData);
-router.put('/:id/lifecycle', protect, authorise('admin', 'staff'), updateComplaintLifecycle);
-router.get('/:id/history', protect, getComplaintHistory);
-router.post('/:id/remarks', protect, authorise('admin', 'staff'), addRemark);
-router.get('/filter', protect, authorise('admin', 'staff'), filterComplaints);
-export default router;
 
+router.get('/', protect, authorise('admin', 'staff'), allComplaints);
+
+
+export default router;
 
 
 
