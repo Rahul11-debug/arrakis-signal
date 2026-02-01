6551bec6b81035421bc6bc7c56b732f8b0599980
manish-core-1 <manish.sevdaundergrade@gmail.com>
2026-02-01 14:25:25 +0530

    login page

--- Frontend/src/pages/auth/Login.jsx
+++ Frontend/src/pages/auth/Login.jsx
@@ -2,11 +2,9 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import Input from "../../components/Input";
 import { loginUser } from "../../api/auth.api";
-import { useAuth } from "../../context/AuthContext";
 
 export default function Login() {
   const navigate = useNavigate();
-  const { login } = useAuth();
 
   const [form, setForm] = useState({
     email: "",
@@ -23,29 +21,30 @@ export default function Login() {
     });
   };
 
-  const handleLogin = async () => {
-    setError("");
+const handleLogin = async (e) => {
+  e.preventDefault();
+  setError("");
 
-    try {
-      setLoading(true);
+  try {
+    setLoading(true);
 
-      const res = await loginUser(form);
+    const res = await loginUser(form);
+    const { token, user } = res.data;
 
-      
-      login(res.data.user);
+    localStorage.setItem("token", token);
+    localStorage.setItem("user", JSON.stringify(user));
 
-     
-      navigate(`/dashboard/${res.data.user.role}`);
+    if (user.role === "citizen") navigate("/dashboard/citizen", { replace: true });
+    if (user.role === "staff") navigate("/dashboard/staff", { replace: true });
+    if (user.role === "admin") navigate("/dashboard/admin", { replace: true });
 
-    } catch (err) {
-      setError(
-        err.response?.data?.message || "Login failed"
-      );
-    } finally {
-      setLoading(false);
-    }
-  };
-
+  } catch (err) {
+    setError(err.response?.data?.message || "Login failed");
+  } finally {
+    setLoading(false);
+  }
+};
+ 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-600 to-purple-700">
       <div className="bg-white backdrop-blur-md rounded-2xl p-5 shadow-2xl text-center w-full max-w-md">
